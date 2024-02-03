5ef57d07f7b7aaeacffdc61e4d5e06fad20a2a20
Matan Polik <polikmatan3@gmail.com>
2024-02-03 16:02:42 +0200

    added test files, test cases. fixed bugs, delete update method in ContactRepository

--- src/main/java/com/example/phone_calls_task_bigid/repository/ContactRepository.java
+++ src/main/java/com/example/phone_calls_task_bigid/repository/ContactRepository.java
@@ -9,17 +9,6 @@ import java.util.Optional;
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, String> {
     /**
-     * Updates the phone number of a contact.
-     *
-     * @param oldPhoneNumber The old phone number to be updated.
-     * @param newPhoneNumber The new phone number to replace the old one.
-     * @return The number of updated contacts (should be 0 or 1).
-     */
-    /*@Modifying
-    @Query("UPDATE Contact c SET c.phoneNumber = :newPhoneNumber WHERE c.phoneNumber = :oldPhoneNumber")
-    int updatePhoneNumber(@Param("oldPhoneNumber") String oldPhoneNumber, @Param("newPhoneNumber") String newPhoneNumber);*/
-
-    /**
      * Finds a contact by their phone number.
      *
      * @param phoneNumber The phone number to search for.
